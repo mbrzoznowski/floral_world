c667daacfd1e4aa465ee1d47d18d002edaf2649d
Michal Brzoznowki <michal.brzoznowski@gmail.com>
2017-05-17 13:05:01 +0200

    Done forms

--- floral_world/app/controllers/orders_controller.rb
+++ floral_world/app/controllers/orders_controller.rb
@@ -6,16 +6,26 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new
+    @order = Order.new(order_params)
     if @order.save
-      flash[:notice] = 'Dodano nowy produkt'
-      redirect_to edit_product_path(Order.where("product_id = ?", (1)))
+      flash[:notice] = 'Dodano nowe kwiaty'
+      puts @order.product_id
+      redirect_to edit_product_path(@order.product_id)
     else
-      flash.now[:error] = 'Dodanie produktu nie powiodło się'
-      redirect_to edit_product_path(Order.where("product_id = ?", (1)))
+      flash[:error] = 'Dodanie kwiatów nie powiodło się'
+      redirect_to edit_product_path(@order.product_id)
+
       # (Order.where(product_id: (params[:id])))
       # (Order.where(product_id: (params[:product_id])))
       # (Product.find(params[:id]))
     end
   end
+
+
+private
+
+  def order_params
+    params.require(:order).permit(:product_id, :material_id, :amount)
+  end
+
 end
