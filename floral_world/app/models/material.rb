a323cd5296c01228d986e0828d779b80ee2d0ceb
Michal Brzoznowki <michal.brzoznowski@gmail.com>
2017-05-19 12:50:40 +0200

    Added validations to models

--- floral_world/app/models/material.rb
+++ floral_world/app/models/material.rb
@@ -1,4 +1,7 @@
 class Material < ApplicationRecord
   has_many :orders
   has_many :products, through: :orders
+
+  validates :name, :price, presence: true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0}
 end
--- floral_world/app/models/order.rb
+++ floral_world/app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :material
   belongs_to :product
+
+
+  validates :material_id, :amount, :product_id, presence: true
+  validates :amount, :numericality => { :only_integer => true }
+
 end
